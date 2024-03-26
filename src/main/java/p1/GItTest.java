ab6f81ac56cb5430b91c6b410556ceb31634b40a
ch <1967635028@qq.com>
2024-03-26 14:52:33 +0800

    hot-fix commit01

--- src/main/java/p1/GItTest.java
+++ src/main/java/p1/GItTest.java
@@ -6,6 +6,5 @@ public class GItTest {
         System.out.println("hello git");
         System.out.println("hello git2...");
         System.out.println("hello git3...");
-        System.out.println("hello git4...");
     }
 }
